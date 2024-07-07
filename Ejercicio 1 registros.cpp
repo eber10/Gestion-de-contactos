0d0b802ac90b0fdba1d39ab02f922521a6b8ef99
ever <eber.penaloza10@gmail.com>
2024-07-06 22:18:39 -0700

    se agrego la opción para agregar un contacto, usando una función

--- Ejercicio 1 registros.cpp
+++ Ejercicio 1 registros.cpp
@@ -12,11 +12,23 @@ struct contactoemail
 	string email;
 	string nacionalidad;
 };
+void registrar_contacto(contactoemail arr[], int &n)
+{
+		cout<<"ingrese nombres completos: "; cin.ignore(); getline(cin, arr[n].nombres);
+		cout<<"ingrese sexo (f/m): "; cin>>arr[n].sexo;
+		cout<<"ingrese edad: "; cin>>arr[n].edad;
+		cout<<"ingrese telefono: "; cin>>arr[n].telefono;
+		cout<<"ingrese email: "; cin>>arr[n].email;
+		cout<<"ingrese nacionalidad: "; cin>>arr[n].nacionalidad;
+		n++;
+		cout<<"contacto agregado exitosamente."<<endl;
+}
 
 
 int main()
 {
 	contactoemail agregar[100];
+	int n=0;
 	char op;
 	do
 	{
@@ -28,6 +40,7 @@ int main()
 		cin>>op;
 		switch(op)
 		{
+			case 'a': registrar_contacto(agregar, n); break;
 			
 		}
 	}
